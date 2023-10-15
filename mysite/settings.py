9c571d02717b0b927ac23ea1d244ab2ffb6de86c
Abhishek Dangi <72682273+Abhi-AD@users.noreply.github.com>
2023-10-15 15:42:31 +0545

    feat;tag and similar post

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libarary
+    'taggit',
     
     # custom app
     'blog.apps.BlogConfig',
